bfa98f200f93288a24eafbb53574866c1201ba9f
jspc <james+git@zero-internet.org.uk>
2023-04-22 13:14:07 +0100

    Simulate game, print output

---
+++ main.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+from game.weapon import *
+from game.player import NPC, Player
+
+from faker import Faker
+import random
+
+players = []
+
+weapons = [
+    Fists(),
+    Handgun(),
+    Knife(),
+    MachinePistol(),
+    CricketBat(),
+]
+
+default_weapon = weapons[0]
+
+fake = Faker()
+
+def simulate_attack(attacker, victim):
+    # A dead attacker can't attack anything, so resurrect
+    # and continue, representing a missed turn
+    if not attacker.alive:
+        attacker.resurrect()
+        return
+
+    # There's no point attacking a dead player, so do nothing
+    if not victim.alive:
+        return
+
+    attacker.attack(victim)
+
+
+def create_player():
+    player = Player(fake.name(), 100, [default_weapon])
+    player.add_weapon(random.sample(weapons, 1)[0])
+    player.equip_weapon(1)
+
+    return player
+
+
+if __name__ == '__main__':
+    # Create a load of NPCS
+    for _ in range(250):
+        players.append(NPC())
+
+
+    # Create a load of players and give them each a random weapon
+    for _ in range(25):
+        players.append(create_player())
+
+
+    # Simulate a load of attacks
+    for _ in range(100_000):
+        attacker = random.sample(players, 1)[0]
+        victim = random.sample(players, 1)[0]
+
+        simulate_attack(attacker, victim)
+
+
+    # Let's have a look at K/Ds for non NPCs
+    for player in players:
+        if player.npc:
+            continue
+
+        print(player)
---
+++ test_main.py
@@ -0,0 +1,43 @@
+import unittest
+
+from main import simulate_attack, create_player
+
+class TestMain(unittest.TestCase):
+    def test_simulate_attack(self):
+        attacker = create_player()
+        attacker.equip_weapon(0)
+
+        victim = create_player()
+
+        simulate_attack(attacker, victim)
+
+        self.assertEqual(victim.alive, True)
+        self.assertEqual(victim.health, 96.6667)
+
+
+    def test_simulate_attack_by_dead_player(self):
+        attacker = create_player()
+        attacker.equip_weapon(0)
+        attacker.die()
+
+        victim = create_player()
+
+        self.assertEqual(attacker.alive, False)
+        simulate_attack(attacker, victim)
+        self.assertEqual(attacker.alive, True)
+
+        self.assertEqual(victim.alive, True)
+        self.assertEqual(victim.health, 100)
+
+
+    def test_simulate_attack_on_dead_player(self):
+        attacker = create_player()
+        attacker.equip_weapon(0)
+
+        victim = create_player()
+        victim.die()
+
+        simulate_attack(attacker, victim)
+
+        self.assertEqual(victim.alive, False)
+        self.assertEqual(victim.health, 0)
